2fee4fbbd274951bf54c93689fb3d4223fce506b
Marti Martinez <marti.martinez7@wework.com>
2024-02-23 23:01:13 +0000

    latest updates

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -f ./Dockerfile -t quay.io/marti_martinez/trfk:main --platform linux/amd64,linux/arm64 --push .
+#docker build -f ./Dockerfile -t quay.io/marti_martinez/trfk:main --platform linux/amd64,linux/arm64 --push .
+docker build -f ./Dockerfile -t quay.io/marti_martinez/trfk:main --push .
